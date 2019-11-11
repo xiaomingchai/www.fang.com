feabe75d39487798baa2abbf61b11a075fc04905
xiaomingchai <1029963569@qq.com>
2019-11-11 09:17:21 +0800

    测试auth

--- app/Http/Controllers/Admin/LoginController.php
+++ app/Http/Controllers/Admin/LoginController.php
@@ -23,7 +23,7 @@ class LoginController extends Controller
             'password'=>'required|min:3|max:12'
         ]);
         //返回true或者false，完事之后如果有的话，返回true，反之false
-        $res=auth()->guard('web')->attempt($data);
+     $res=auth()->guard('web')->attempt($data);
         //检查是否成功登录，成功true，失败false
         $che=auth()->check();
         //登陆成功后返回用户信息,,没有的话返回null
